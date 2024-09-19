c085c9c4cc4f9a7b5213629618c4b54ce70c952b
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-09-20 01:53:33 +0300

    CategoryService and Repository added.

---
+++ LibraryManagement.ConsoleUI/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagement.ConsoleUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.ConsoleUI.Repository
+{
+    public class CategoryRepository
+    {
+        List<Category> categories = new List<Category>()
+        {
+            new Category (1,"Dünya Klasikleri"),
+            new Category (2,"Türk Klasikleri"),
+            new Category (3,"Bilim Kurgu"),
+        };
+
+        public List<Category> GetAll()
+        {
+            return categories;
+        }
+    }
+}
