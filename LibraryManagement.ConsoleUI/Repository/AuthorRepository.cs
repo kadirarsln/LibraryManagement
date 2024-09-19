08a0f55e3f283ebee2def16c24f4f7c66fdac1ef
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-09-20 01:53:56 +0300

    AuthorService and Repository added

---
+++ LibraryManagement.ConsoleUI/Repository/AuthorRepository.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagement.ConsoleUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.ConsoleUI.Repository
+{
+    public class AuthorRepository
+    {
+        List<Author> authors = new List<Author>()
+        {
+            new Author(1,"Emile","Zola"),
+            new Author(2,"Fyodor","Dostoyevski"),
+            new Author(3,"Recaizade Mahmut","Ekrem"),
+            new Author(4,"Halide Edib","Adıvar"),
+            new Author(5,"Ömer","Seyfettin"),
+            new Author(5,"Ali","Koç"),
+            new Author(6,"Vız Vız","Ali")
+        };
+
+        public List<Author> GetAll() { return authors; }
+    }
+}
